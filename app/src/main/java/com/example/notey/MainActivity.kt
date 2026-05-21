1bc77503d29699d58dcf1acc9dca83275bc62732
abhinichi <abhijit.behera@nichi.com>
2026-05-21 15:42:12 +0530

    adding note feature added - color pick option need to be added

--- app/src/main/java/com/example/notey/MainActivity.kt
+++ app/src/main/java/com/example/notey/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.notey
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
 import androidx.core.graphics.toColorInt
 import androidx.lifecycle.ViewModelProvider
 import com.example.notey.repository.NotesRepository
 import com.example.notey.roomdb.Note
 import com.example.notey.roomdb.NotesDB
+import com.example.notey.screen.DisplayDialog
 import com.example.notey.screen.DisplayNotesList
 import com.example.notey.ui.theme.NoteyTheme
 import com.example.notey.viewModel.NoteViewModel
@@ -18,6 +30,7 @@ import com.example.notey.viewModel.NoteViewModelFactory
 import kotlin.jvm.java
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -42,9 +55,36 @@ class MainActivity : ComponentActivity() {
         noteViewModel.insert(note1)
         setContent {
             NoteyTheme {
-                val notes by noteViewModel.allNotes.collectAsState(emptyList())
-                DisplayNotesList(notes = notes)
+                Scaffold(floatingActionButton = {
+                    MyFlotActBtn(
+                        viewModel = noteViewModel
+                    )
+                }) {
+                    val notes by noteViewModel.allNotes.collectAsState(emptyList())
+                    DisplayNotesList(notes = notes)
+                }
             }
         }
+    }
+}
+
+@Composable
+fun MyFlotActBtn(viewModel: NoteViewModel){
+
+    var showDialog by remember {
+        mutableStateOf(false)
+    }
+
+    DisplayDialog(viewModel = viewModel, showDialog = showDialog) {
+        showDialog = false
+    }
+
+    FloatingActionButton(
+        onClick = {showDialog = true},
+        containerColor = Color.DarkGray,
+        contentColor = Color.White
+        ) {
+        Icon(imageVector = Icons.Filled.Add,
+            contentDescription = "Add Note")
     }
 }
---
+++ app/src/main/java/com/example/notey/screen/DisplayDialog.kt
@@ -0,0 +1,72 @@
+package com.example.notey.screen
+
+import android.app.AlertDialog
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.dp
+import com.example.notey.roomdb.Note
+import com.example.notey.viewModel.NoteViewModel
+import java.nio.file.WatchEvent
+
+@Composable
+fun DisplayDialog(viewModel: NoteViewModel, showDialog: Boolean, onDismiss: () -> Unit) {
+    var title by remember { mutableStateOf("") }
+    var description  by remember { mutableStateOf("") }
+    var selectedColor by remember { mutableStateOf(Color.Blue) }
+
+    if(showDialog){
+    AlertDialog(
+        onDismissRequest = onDismiss ,
+        title = { Text(text = "Add Note") },
+        text = {
+            Column() {
+                TextField(
+                    value = title,
+                    onValueChange = { title = it },
+                    label = { Text("Note Title") }
+                )
+                Spacer(
+                    modifier = Modifier.height(14.dp)
+                )
+                TextField(
+                    value = description,
+                    onValueChange = { description = it },
+                    label = { Text("Note Description") }
+                )
+                Spacer(
+                    modifier = Modifier.height(14.dp)
+                )
+            }
+        },
+        confirmButton = {
+            Button(onClick = {
+                    val newNote = Note(
+                        id = 0,
+                        title = title,
+                        description = description,
+                        color = selectedColor.toArgb()
+                    )
+                    viewModel.insert(newNote)
+            }) { Text(text = "Save Note") }
+        },
+        dismissButton = {
+   Button(onClick = onDismiss
+   ) { Text( text = "Cancel") }
+        }
+    )}
+
+}
